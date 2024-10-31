a8964911e9ad2ffb3b9ac646b248ef3e1c28bdb6
Pierpaolo Calanna <p.calanna@gmail.com>
2024-10-31 10:17:34 +0100

    added roles in the match section

--- assets/js/alpine/views/match.convergence.js
+++ assets/js/alpine/views/match.convergence.js
@@ -2,11 +2,27 @@ export default () => ({
   
   initMatchConvergence() {},
 
+  groupMe: {
+    ["x-ref"]: "groupMe",
+
+    ["x-text"]() {
+      return this.$store.keirsey.groupMe
+    }
+  },
+
   roleMe: {
     ["x-ref"]: "roleMe",
 
     ["x-text"]() {
       return this.$store.keirsey.roleMe
+    }
+  },
+
+  groupYou: {
+    ["x-ref"]: "groupYou",
+
+    ["x-text"]() {
+      return this.$store.keirsey.groupYou
     }
   },
 
