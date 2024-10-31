a9e2dfbfc720bff6aa675e207b723eba660c9d88
Pierpaolo Calanna <p.calanna@gmail.com>
2024-10-31 13:03:50 +0100

    implementing compare answers

--- assets/js/alpine/views/match.compare-answers.js
+++ assets/js/alpine/views/match.compare-answers.js
@@ -1,22 +1,27 @@
 export default () => ({
 
   itemsWithAnswers: [],
-  show: "all",
+  showList: ["all", "same", "different"],
+  showIndex: 0,  
 
   initMatchCompareAnswers() {
     this.itemsWithAnswers = this.$store.answers.itemsWithAnswers;
-    this.$watch("show", (val, oldVal) => {
+    this.$watch("showIndex", (val, oldVal) => {
       if (val !== oldVal) {
-        val === "all" 
+        val === 0 
           && (this.itemsWithAnswers = this.$store.answers.itemsWithAnswers)
-        val === "same" 
+        val === 1 
           && (this.itemsWithAnswers = this.$store.answers.itemsWithAnswers
             .filter(el => el.answerMe.answerValue === el.answerYou.answerValue))
-        val === "different" 
+        val === 2 
           && (this.itemsWithAnswers = this.$store.answers.itemsWithAnswers
             .filter(el => el.answerMe.answerValue !== el.answerYou.answerValue))
       }
     })
+  },
+
+  get show() {
+    return this.showList[this.showIndex]
   },
 
   items: {
